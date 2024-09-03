6d2f71f53056f0657cec5242938ccfbcd69fb9cb
Nagarjunl <naga.career.at@gmail.com>
2024-09-03 10:33:02 +0530

    Added Javasscript

---
+++ Javascript/variables/var.js
@@ -0,0 +1,23 @@
+var age; //Declaration
+
+var age = 20; //Declaration + Definition
+
+age = 30; //Mutate the original value
+
+var name = 'Sathish'; // or "Sathish"
+
+var eligibleToVote = true; // or false,  1 - is equalent to true and 0 -is equalent to false
+
+// Datatypes
+
+// Primitive DataTypes
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Bigint
+// 7. Symbol
+
+// Non - Primitive DataTypes
+1. Array
