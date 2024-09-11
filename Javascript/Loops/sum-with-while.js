72557575690515fe2c3a4542f26785fc65b2d071
Sathishkumar R <sathishkumar@sivisoft.com>
2024-09-11 11:52:29 +0530

    Added sum with for and while

--- Javascript/Loops/sum-with-while.js
+++ Javascript/Loops/sum-with-while.js
@@ -6,10 +6,7 @@ const marks = [85, 73, 80, 81, 98]; // length = 5
 
 let total = 0; //417
 
-// Use for loop to iterate over the marks array and calculate sum by adding current index value
+// Use while loop to iterate over the marks array and calculate sum by adding current index value
 // and add the current value to above created total varible repeatedly
-for (let i = 0; i < marks.length; i++) {
-  total = total + marks[i];
-}
 
 console.log(total);
